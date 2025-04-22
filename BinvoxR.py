28f0e40ddcf09340cb8a0e5ef666294f4ac92621
OmarMuro18 <omarmuro117@gmail.com>
2025-04-22 12:15:33 -0600

    Implementacion de operaciones morfologicas para aplicar en imagenes

---
+++ BinvoxR.py
@@ -0,0 +1,106 @@
+import numpy as np
+
+class Voxels(object):
+
+    def __init__(self, data, dims, translate, scale, axis_order):
+        self.data = data
+        self.dims = dims
+        self.translate = translate
+        self.scale = scale
+        assert (axis_order in ('xzy', 'xyz'))
+        self.axis_order = axis_order
+
+    def clone(self):
+        data = self.data.copy()
+        dims = self.dims[:]
+        translate = self.translate[:]
+        return Voxels(data, dims, translate, self.scale, self.axis_order)
+
+    def write(self, fp):
+        write(self, fp)
+
+
+def read_header(fp):
+    """ Read binvox header. Mostly meant for internal use.
+    """
+    line = fp.readline().strip()
+    if not line.startswith(b'#binvox'):
+        raise IOError('Not a binvox file')
+    dims = list(map(int, fp.readline().strip().split(b' ')[1:]))
+
+    translate = list(map(float, fp.readline().strip().split(b' ')[1:]))
+    scale = list(map(float, fp.readline().strip().split(b' ')[1:]))[0]
+    line = fp.readline()
+    return dims, translate, scale
+
+def read(fp,  fix_coords=True):
+    dims, translate, scale = read_header(fp)
+    raw_data = np.frombuffer(fp.read(), dtype=np.uint8)
+
+    values, counts = raw_data[::2], raw_data[1::2]
+    data = np.repeat(values, counts).astype(np.bool)
+    data = data.reshape(dims)
+    if fix_coords:
+        # xzy to xyz TODO the right thing
+        data = np.transpose(data, (0, 2, 1))
+        axis_order = 'xyz'
+    else:
+        axis_order = 'xzy'
+    return Voxels(data, dims, translate, scale, axis_order)
+
+def write(voxel_model, fp):
+    if voxel_model.data.ndim==2:
+        # TODO avoid conversion to dense
+        dense_voxel_data = sparse_to_dense(voxel_model.data, voxel_model.dims)
+    else:
+        dense_voxel_data = voxel_model.data
+
+    fp.write('#binvox 1\n')
+    fp.write('dim '+' '.join(map(str, voxel_model.dims))+'\n')
+    fp.write('translate '+' '.join(map(str, voxel_model.translate))+'\n')
+    fp.write('scale '+str(voxel_model.scale)+'\n')
+    fp.write('data\n')
+    if not voxel_model.axis_order in ('xzy', 'xyz'):
+        raise ValueError('Unsupported voxel model axis order')
+
+    if voxel_model.axis_order=='xzy':
+        voxels_flat = dense_voxel_data.flatten()
+    elif voxel_model.axis_order=='xyz':
+        voxels_flat = np.transpose(dense_voxel_data, (0, 2, 1)).flatten()
+
+    # keep a sort of state machine for writing run length encoding
+    state = voxels_flat[0]
+    ctr = 0
+    for c in voxels_flat:
+        if c==state:
+            ctr += 1
+            # if ctr hits max, dump
+            if ctr==255:
+                fp.write(chr(state))
+                fp.write(chr(ctr))
+                ctr = 0
+        else:
+            # if switch state, dump
+            fp.write(chr(state))
+            fp.write(chr(ctr))
+            state = c
+            ctr = 1
+    # flush out remainders
+    if ctr > 0:
+        fp.write(chr(state))
+        fp.write(chr(ctr))
+
+def sparse_to_dense(voxel_data, dims, dtype=np.bool):
+    if voxel_data.ndim!=2 or voxel_data.shape[0]!=3:
+        raise ValueError('voxel_data is wrong shape; should be 3xN array.')
+    if np.isscalar(dims):
+        dims = [dims]*3
+    dims = np.atleast_2d(dims).T
+    # truncate to integers
+    xyz = voxel_data.astype(np.int)
+    # discard voxels that fall outside dims
+    valid_ix = ~np.any((xyz < 0) | (xyz >= dims), 0)
+    xyz = xyz[:,valid_ix]
+    out = np.zeros(dims.flatten(), dtype=dtype)
+    out[tuple(xyz)] = True
+    return out
